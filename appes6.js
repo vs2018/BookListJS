48285b98082dcf9526e48b7386bf17c1933d383d
Vishal Sharma <vsh1819@gmail.com>
2018-04-02 15:52:21 +0100

    created a Local Storage class with 4 methods to persist the data

--- appes6.js
+++ appes6.js
@@ -58,6 +58,57 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks(){
+    let books
+    if(localStorage.getItem('books') === null){
+      books = []
+    } else {
+      books = JSON.parse(localStorage.getItem('books'))
+    }
+
+    return books
+  }
+
+  static displayBooks(){
+    const books = Store.getBooks()
+
+    books.forEach(function(book){
+      const ui = new UI
+
+      // Add book to UI
+      ui.addBookToList(book)
+    })
+  }
+
+  static addBook(book){
+    const books = Store.getBooks()
+
+    books.push(book)
+
+    localStorage.setItem('books', JSON.stringify(books))
+
+  }
+
+  static removeBook(isbn){
+    const books = Store.getBooks()
+
+    books.forEach(function(book, index){
+      if(book.isbn === isbn){
+        books.splice(index, 1)
+      }
+    })
+
+    localStorage.setItem('books', JSON.stringify(books))
+
+    
+  }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks)
+
 // Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
   // Get Form values
@@ -78,6 +129,9 @@ document.getElementById('book-form').addEventListener('submit', function(e){
   } else {
     // Add book to list
     ui.addBookToList(book)
+
+    // Add To LS
+    Store.addBook(book)
 
     // Show successs
 
@@ -101,6 +155,9 @@ document.getElementById('book-list').addEventListener('click', function(e){
   const ui = new UI()
 
   ui.deleteBook(e.target)
+
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
   // Show message
   ui.showAlert('Book Removed!', 'success')
